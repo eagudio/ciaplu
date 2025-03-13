4a75866a6109a9ac11e763c7ecf8f28cf58cd998
eagudio <edoardoagudio@gmail.com>
2025-03-13 10:32:05 +0100

    feat: export Context class from main module

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Matcher } from "./matcher/matcher";
+import { Context } from "./matcher/statements/context";
 
 export const match = (value: any) => new Matcher(value)
-export { Matcher };
+export { Matcher, Context };
